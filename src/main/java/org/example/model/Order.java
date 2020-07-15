e6cc86f254e49d8a51377b0a82c4acc2eaa8cd1b
CptJackFrost <cptjackfrost74@gmail.com>
2020-07-15 16:23:32 +0500

    Order and test classes. Properties.

---
+++ src/main/java/org/example/model/Order.java
@@ -0,0 +1,99 @@
+package org.example.model;
+
+import com.google.gson.annotations.SerializedName;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.Objects;
+
+public class Order {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("petId")
+    private int petId;
+
+    @SerializedName("quantity")
+    private int quantity;
+
+    @SerializedName("shipDate")
+    private String shipDate;
+
+    @SerializedName("status")
+    private String status;
+
+    @SerializedName("complete")
+    private boolean complete;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getShipDate() {
+        return shipDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setShipDate(String shipDate) {
+        this.shipDate = shipDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, petId, quantity, shipDate, status, complete);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj.getClass() != this.getClass() || obj == null) {
+            return false;
+        }
+        Order order = (Order) obj;
+        return id == order.id &&
+                petId == order.petId &&
+                quantity == order.quantity &&
+                shipDate == order.shipDate &&
+                status == order.status &&
+                complete == order.complete;
+    }
+
+
+}
